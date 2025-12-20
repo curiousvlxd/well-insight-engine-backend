47526feb1b04b193adc3c19b90240cb64e5ead13
Vlad <vlxdtimchenko@gmail.com>
2025-12-20 15:25:20 +0200

    feat: GetAllowedEmails endpoint

---
+++ src/WellInsightEngine.Api/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WellInsightEngine.Core.Features.Assets.GetAssets;
+using WellInsightEngine.Infrastructure.Services.Auth.Options;
+
+namespace WellInsightEngine.Api.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class UsersController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<GetAssetsResponse> GetAllowedEmails([FromServices] IOptions<KlerkAuthorizationOptions> options, CancellationToken cancellation)
+    {
+        var result = options.Value.AllowedEmails;
+        return Ok(result);
+    }
+}
